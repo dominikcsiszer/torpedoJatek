36f0f90fb282841773c9005cf2647c0dcd15ed8e
SZF1 A1 <szf1 a1@oktatas.local>
2022-05-23 12:37:04 +0200

    tesztLoves methodus kész

--- src/main/TorpedoTeszt.java
+++ src/main/TorpedoTeszt.java
@@ -2,10 +2,15 @@ package main;
 
 public class TorpedoTeszt {
     public static void main(String[] args) {
+        new TorpedoTeszt().tesztLoves(4);
         System.out.println("Teszt");
+//        assert 3==5 : "Nem jó a találat ellenőrzése.";
     }
     
     public String tesztLoves(int poz) {
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(poz);
+        assert t.equals("talált") : "Nem jó a találat ellenőrzés.";
         return "";
     }
 }
